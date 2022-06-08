759eeae7e3e24c4d81860efc7685f0d9ad6ab9ec
kygx-legend <kygx.legend@gmail.com>
2022-06-08 18:17:58 +0800

    Support t4

--- src/common/meta.h
+++ src/common/meta.h
@@ -26,9 +26,9 @@ const static size_t MAX_THREADS_NUM = MAX_BLOCKS_NUM * THREADS_PER_BLOCK;
 
 const static unsigned int FULL_WARP_MASK = 0xffffffff;
 
-const static double kDeviceMemoryUnit = 10.6;  // 2080ti
+//const static double kDeviceMemoryUnit = 10.6;  // 2080ti
 // const static double kDeviceMemoryUnit = 15.5;  // v100
-// const static double kDeviceMemoryUnit = 14.5;  // t4
+const static double kDeviceMemoryUnit = 14.5;  // t4
 // const static double kDeviceMemoryUnit = 10.357;  // alignment
 const static size_t kDeviceMemoryLimits[8] = {(size_t)(kDeviceMemoryUnit * 1024 * 1024 * 1024), (size_t)(kDeviceMemoryUnit * 1024 * 1024 * 1024), (size_t)(kDeviceMemoryUnit * 1024 * 1024 * 1024),
                                               (size_t)(kDeviceMemoryUnit * 1024 * 1024 * 1024), (size_t)(kDeviceMemoryUnit * 1024 * 1024 * 1024), (size_t)(kDeviceMemoryUnit * 1024 * 1024 * 1024),
